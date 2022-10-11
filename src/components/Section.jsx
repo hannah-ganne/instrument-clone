e17bf5299a93330e463cc2cbfa69c95bdf2468b0
lanomade <ganne.hannah@gmail.com>
2022-10-11 12:01:43 +0200

    add css transition in Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,14 @@
 import "../utils/styles/Section.css"
+import { useRef, useState, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 export default function Section({ children }) {
+    const { ref: containerRef, inView: isVisible } = useInView({
+        threshold: 0.3
+    }) 
+
     return (
-        <section>
+        <section ref={containerRef} className={isVisible? 'fade-in' : ''}>
             {children}
         </section>
     )

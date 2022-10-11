a12f13ae436c245156798e953df40357204afefd
lanomade <ganne.hannah@gmail.com>
2022-10-11 12:00:58 +0200

    put in place sticky header on scroll up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
 import Nav from './Nav'
 import NavItem from './NavItem'
 import '../utils/styles/Header.css'
+import { useEffect } from 'react'
 
 export default function Header() {
+
+    useEffect(() => {
+        const body = document.body
+        const scrollUp = "scroll-up"
+        const scrollDown = "scroll-down"
+        let lastScroll = 0;
+        
+        window.addEventListener("scroll", () => {
+            const currentScroll = window.pageYOffset;
+
+            if (currentScroll <= 83) {
+                body.classList.remove(scrollUp);
+
+                return;
+            }
+
+            if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+              // down
+                body.classList.remove(scrollUp);
+                body.classList.add(scrollDown);
+
+            } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+              // up
+                body.classList.remove(scrollDown);
+                body.classList.add(scrollUp);
+            }
+
+            lastScroll = currentScroll;
+        });
+    })
+
     return (
-        <header>
+        <header className="header">
             <a href="#">
                 <p>INSTRUMENT</p>
             </a>
